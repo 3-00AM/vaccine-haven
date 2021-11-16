5d9605a03906e700a41f96072e7bbfc92d1460e1
Narawish Sangsiriwut <narawish.s@ku.th>
2021-11-16 22:51:59 +0700

    add logo to the website

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {Link} from "react-router-dom";
 import 'cirrus-ui';
-
+import homeLogo from '../img/homeLogo.png';
 
 function Navbar(props) {
 
@@ -14,13 +14,17 @@ function Navbar(props) {
     <div
       className={`header header-fixed unselectable header-animated doc-header ${isActive ? "" : "translucent"} ${props.onScroll || "header-dark"}`}>
       <div className="header-brand">
-        <div className="nav-item no-hover">
-          <Link to="/"><h6 className="title white">Vaccine Haven</h6></Link>
+
+        <div className="nav-item no-hover pl-0" style={{width: "75%", justifyContent: "flex-start"}}>
+          <Link to="/">
+            <img src={homeLogo} alt="homeLogo"/>
+            {/*<h6 className="title white">Vaccine Haven</h6>*/}
+          </Link>
         </div>
         <div onClick={ToggleClass} className={`nav-item nav-btn ${isActive ? "" : "active"}`} id={`header-btn`}>
-          <span />
-          <span />
-          <span />
+          <span/>
+          <span/>
+          <span/>
         </div>
       </div>
       <div className={`header-nav ${isActive ? "" : "active"}`} id={`header-menu`}>
