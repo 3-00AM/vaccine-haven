506c4e9315b41034bde0ae1291a8499e6ddcba04
Narawish Sangsiriwut <narawish.s@ku.th>
2021-11-23 15:03:36 +0700

    Add Click bait

--- src/Components/WalkIn.js
+++ src/Components/WalkIn.js
@@ -7,6 +7,7 @@ import Navbar from "./Navbar";
 export default function WalkIn() {
 
   const [site, setSite] = useState([]);
+  const [youtube, setMedia] = useState(null);
 
   const config = {
     method: 'GET',
@@ -28,16 +29,31 @@ export default function WalkIn() {
     console.log(site)
   }, []);
 
+  const yeet = () => {
+    setMedia(
+      <div className="media-stretch rat-4-3">
+        <iframe width="" height="" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"
+                title="YouTube video player" frameBorder="0" allow='autoplay'/>
+      </div>)
+  }
+
   return <div className="bg-blue-100">
     <Navbar/>
     <div className="py-10">
+      {youtube}
+      <div className="p-3">
+        <div className="toast toast--danger animated fadeIn infinite">
+          <button className="btn-close" onClick={yeet}/>
+          <p>Error</p>
+        </div>
+      </div>
       {site.map(site => {
         return <div className="p-3">
           <div className="card p-3">
             <div className="card-body">
               Site: {site.name}
               <Link to="/" style={{float: "right"}} className="utb utb-OLR"> More Info </Link>
-              <div className="animated loading"> </div>
+              <div className="animated loading"/>
             </div>
             <div className="card-footer">
               Location: {site.location}
