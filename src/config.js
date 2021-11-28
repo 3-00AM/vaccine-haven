095cd3c610897d5a4b54ab9d7143bed5e0b0eb9e
Narawish Sangsiriwut <narawish.s@ku.th>
2021-11-28 13:51:50 +0700

    otp firebase

---
+++ src/config.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/compat"
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBvKJntOjUGr21Tmxp-G0Ey1w1DwEB5Wfs",
+  authDomain: "vaccinehaven.firebaseapp.com",
+  projectId: "vaccinehaven",
+  storageBucket: "vaccinehaven.appspot.com",
+  messagingSenderId: "703633343525",
+  appId: "1:703633343525:web:8168f9572b4972af682bcc"
+});
+
+export default firebase;
