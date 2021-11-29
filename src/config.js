657eb5e90fc3987efd2f80db904f3adc3ed47a6f
Narawish Sangsiriwut <narawish.s@ku.th>
2021-11-29 20:48:26 +0700

    edit var

--- src/config.js
+++ src/config.js
@@ -3,13 +3,13 @@ import 'firebase/compat/auth'
 import 'firebase/compat/firestore'
 
 firebase.initializeApp({
-  apiKey: "AIzaSyBvKJntOjUGr21Tmxp-G0Ey1w1DwEB5Wfs",
-  authDomain: "vaccinehaven.firebaseapp.com",
-  projectId: "vaccinehaven",
-  storageBucket: "vaccinehaven.appspot.com",
-  messagingSenderId: "703633343525",
-  appId: "1:703633343525:web:8168f9572b4972af682bcc",
-  measurementId: "G-RFLKJEENW9",
+  apiKey:             process.env.REACT_APP_API_KEY,
+  authDomain:         process.env.REACT_APP_AUTH_DOMAIN,
+  projectId:          process.env.REACT_APP_PROJECT_ID,
+  storageBucket:      process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:  process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId:              process.env.REACT_APP_APP_ID,
+  measurementId:      process.env.REACT_APP_MEASUREMENT_ID,
 });
 
 export const db = firebase.firestore();
