15b50def9e779d04a5855871991499b7df216899
Vichisorn <james31366@gmail.com>
2021-12-04 10:21:19 +0700

    fix url to BASE_URL in getAccessToken

--- src/lib/getAccessToken.js
+++ src/lib/getAccessToken.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {config} from "../utils";
+import {BASE_URL, config} from "../utils";
 import axios from "axios";
 
 export const getAccessToken = async () => {
-  await axios.post(`https://wcg-apis-test.herokuapp.com/login`, null, {
+  await axios.post(`${BASE_URL}/login`, null, {
     headers: {
       'Authorization': 'Basic ' + process.env.REACT_APP_AUTH_DATA
     }
